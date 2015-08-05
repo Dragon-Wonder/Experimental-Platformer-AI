7a94ce2f95f506546aec121d5d0284fe683ba811
GamerMan7799 <GamerMan7799@users.noreply.github.com>
2015-08-04 20:08:32 -0400

    v3.2.0

--- complie.sh
+++ complie.sh
@@ -1,13 +1,13 @@
-rm marioexperiment.exe
+rm Platformer-Experiment.exe
 cd ./Resources/
 windres my_icon.rc -O coff my_icon.res
 windres boilerplate.rc -O coff boilerplate.res
 cd ../Source/
-g++ -Wall -g -c mario.cpp
+g++ -Wall -g -c platformer.cpp
 g++ -Wall -g -c config.cpp
 cd ../
-g++ -g -o marioexperiment.exe ./Source/mario.o ./source/config.o ./Resources/my_icon.res ./Resources/boilerplate.res
+g++ -g -o Platformer-Experiment.exe ./Source/platformer.o ./source/config.o ./Resources/my_icon.res ./Resources/boilerplate.res
 rm ./Resources/boilerplate.res
 rm ./Resources/my_icon.res
-rm ./Source/mario.o
+rm ./Source/platformer.o
 rm ./Source/config.o
