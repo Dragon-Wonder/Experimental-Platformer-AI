1edf1ff2cf2a22310ec686fb82c3e9da2619deeb
Patrick Rye <ryepatrick@gmail.com>
2015-09-01 11:37:36 -0400

    File moving
    Moved some files around.
    Updated Compile.sh and Object-Builder.sh
    Moved Compile-Debug & Compile-Final to Debug Folder as I cannot get them
    working.
    Added Cleanup.bat which deletes a bunch of files that aren't needed.

--- Debug/complie.sh
+++ Debug/complie.sh
@@ -7,25 +7,25 @@ rm Platformer-Experiment-PRIVATE.exe
 #Move to the Resources directory
 cd ./Resources/
 #Complie the resource files of the icon and boilerplate
-windres my_icon.rc -O coff my_icon.res
 windres boilerplate.rc -O coff boilerplate.res
 #Move to the source folder
 cd ../Source
 #Complie each of the cpp files
-g++ -std=c++11 -Wall -Wextra -pg -g -c main.cpp
-g++ -std=c++11 -Wall -Wextra -pg -g -c config.cpp
-g++ -std=c++11 -Wall -Wextra -pg -g -c map.cpp
-g++ -std=c++11 -Wall -Wextra -pg -g -c entity.cpp
-g++ -std=c++11 -Wall -Wextra -pg -g -c tick.cpp
+g++ -std=c++11 -I../SDL2Stuff/include -Wall -Wextra -pg -g -c main.cpp
+g++ -std=c++11 -I../SDL2Stuff/include -Wall -Wextra -pg -g -c config.cpp
+g++ -std=c++11 -I../SDL2Stuff/include -Wall -Wextra -pg -g -c map.cpp
+g++ -std=c++11 -I../SDL2Stuff/include -Wall -Wextra -pg -g -c entity.cpp
+g++ -std=c++11 -I../SDL2Stuff/include -Wall -Wextra -pg -g -c tick.cpp
+g++ -std=c++11 -I../SDL2Stuff/include -Wall -Wextra -pg -g -c screen.cpp
 #Move back to the main directory
 cd ../
 #Complie everything together!
-g++ -std=c++11 -Wall -Wextra -pg -g -o Platformer-Experiment-PRIVATE.exe ./Source/main.o ./Source/config.o ./Source/map.o ./Source/entity.o ./Source/tick.o ./Resources/my_icon.res ./Resources/boilerplate.res
+g++ -std=c++11 -L./SDL2Stuff/lib -Wall -Wextra -pg -g -o PlatformerExperiment-PRIVATE.exe ./Source/main.o ./Source/config.o ./Source/map.o ./Source/entity.o ./Source/tick.o ./Source/screen.o ./Resources/boilerplate.res -lSDL2main -lSDL2
 #Delete all the leftover parts
 rm ./Resources/boilerplate.res 
-rm ./Resources/my_icon.res 
 rm ./Source/main.o 
 rm ./Source/config.o 
 rm ./Source/map.o 
 rm ./Source/entity.o 
 rm ./Source/tick.o 
+rm ./Source/screen.o
